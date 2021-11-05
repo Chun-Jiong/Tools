73a8f9762fd46977cd6fb57883430b16229cbc82
Chun-Jiong Huang <chunjiong.huang@gmail.com>
2021-11-05 14:06:36 +0800

    add GN to let it be suitable for imaginary-tau data
    modify:
       spin_structure_factor/SSF.f90
    
    [ticket: normal]

--- spin_structure_factor/SSF.f90
+++ spin_structure_factor/SSF.f90
@@ -1,6 +1,12 @@
 ! image data
 ! infile's format
 !
+! 0  !also could be other integer number
+! real(SqSq(0,0,0))  aimag(SqSq(0,0,0))
+! real(SqSq(1,0,0))  aimag(SqSq(1,0,0))
+! real(SqSq(2,0,0))  aimag(SqSq(2,0,0))
+! ...
+! 0
 ! real(SqSq(0,0,0))  aimag(SqSq(0,0,0))
 ! real(SqSq(1,0,0))  aimag(SqSq(1,0,0))
 ! real(SqSq(2,0,0))  aimag(SqSq(2,0,0))
@@ -12,7 +18,8 @@ program structure_factor
 	integer(4)                  :: narg                 ! the number of input argument
 
 	character(50)               :: filename
-	complex(8), allocatable     :: SqSq(:,:,:,:,:)
+	integer(4)                  :: GN
+	complex(8), allocatable     :: SqSq(:,:,:,:,:,:)
 	real(8)                     :: q(3), dq(3)
 	integer(4)                  :: Lx, Ly, Lz
 	integer(4)                  :: subl
@@ -23,7 +30,8 @@ program structure_factor
 	real(8)                     :: reclatvec(3,3)
 	real(8)                     :: sublatvec(3,3)
 	integer(4)                  :: nperiod
-	character(50)               :: charL, charformat
+	character(50)               :: charnum, charformat
+	integer(4)                  :: iGN
 	integer(4)                  :: ix, iy, iz
 	integer(4)                  :: sb1, sb2
 	real(8)                     :: clx(2)
@@ -33,30 +41,32 @@ program structure_factor
 	integer(4)                  :: i
 
 	narg = command_argument_count()
-	if( narg<6 ) then
-		write(*,*) "Please enter: filename, Lx, Ly, Lz, subl, nperiod; fm"
+	if( narg<7 ) then
+		write(*,*) "Please enter: filename, GN, Lx, Ly, Lz, subl, nperiod; fm"
 		stop
 	end if
 
 	call getarg(1, filename)
-	call getarg(2, charL)
-	read(charL,*) Lx
-	call getarg(3, charL)
-	read(charL,*) Ly
-	call getarg(4, charL)
-	read(charL,*) Lz
-	call getarg(5, charL)
-	read(charL,*) subl
-	call getarg(6, charL)
-	read(charL,*) nperiod
-	if( narg>6 ) then
-		call getarg(7, charL)
-		read(charL,*) fm      ! 1: keep q=0;  0: set S(q=0)=0
+	call getarg(2, charnum)
+	read(charnum,*) GN
+	call getarg(3, charnum)
+	read(charnum,*) Lx
+	call getarg(4, charnum)
+	read(charnum,*) Ly
+	call getarg(5, charnum)
+	read(charnum,*) Lz
+	call getarg(6, charnum)
+	read(charnum,*) subl
+	call getarg(7, charnum)
+	read(charnum,*) nperiod
+	if( narg>7 ) then
+		call getarg(8, charnum)
+		read(charnum,*) fm      ! 1: keep q=0;  0: set S(q=0)=0
 	else
 		fm=1
 	end if
 
-	allocate(SqSq(subl,subl,0:Lx-1,0:Ly-1,0:Lz-1))
+	allocate(SqSq(0:GN,subl,subl,0:Lx-1,0:Ly-1,0:Lz-1))
 
 	reclatvec = 0.d0
 	open(1,file="reclatvec.dat",action="read")
@@ -78,25 +88,34 @@ program structure_factor
 	ndata = 0
 	open(1,file=trim(filename)//".txt",action="read")
 	do
+		read(1,*,end=10) i
 		do iz=0, Lz-1; do iy=0, Ly-1; do ix=0, Lx-1
-			do sb2=1, subl; do sb1=1,subl      !!!!! sb2, sb1
-				read(1,*,end=10) clx(1), clx(2)
-				SqSq(sb1,sb2,ix,iy,iz) = SqSq(sb1,sb2,ix,iy,iz) + CMPLX(clx(1),clx(2))
-				ndata = ndata+1
-			end do; end do
+			do iGN=0, GN
+				do sb2=1, subl; do sb1=1,subl      !!!!! sb2, sb1
+					read(1,*) clx(1), clx(2)
+					SqSq(iGN,sb1,sb2,ix,iy,iz) = SqSq(iGN,sb1,sb2,ix,iy,iz) + CMPLX(clx(1),clx(2))
+					ndata = ndata+1
+				end do; end do
+			end do
 		end do; end do; end do
 		NBlck = NBlck+1
 	end do
 	10 close(1)
-	if( ndata /= (Lx*Ly*Lz*subl*subl)*NBlck ) then
+	if( ndata/=(Lx*Ly*Lz*subl*subl)*NBlck*(GN+1) .or. &
+		ix/=Lx .or. iy/=Ly .or. iz/=Lz .or. &
+		sb1/=subl+1 .or. sb2/=subl+1 .or. &
+		iGN/=GN+1 ) then
 		write(*,*) "Err: the data is incomplete."
 		stop
 	end if
+	!write(*,*) ndata, ix, iy, iz, sb1, sb2, iGN
+	write(*,'(A10,I6)') "NBlck = ", NBlck
 
 	SqSq = SqSq/(NBlck*1.d0)
 
 	open(1,file=trim(filename)//".dat",action="write")
 	open(2,file=trim(filename)//"-single.dat",action="write")
+	if( GN>0 ) open(3,file=trim(filename)//"-tau.dat",action="write")
 	if( Lz==1 ) then
 		m1=0; m2=0
 	else
@@ -107,6 +126,7 @@ program structure_factor
 		do h=-nperiod*Lx+1, nperiod*Lx-1
 	!do l=0, nperiod*Ly-1
 	!	do h=0, nperiod*Lx-1
+			iGN=0
 			q = reclatvec(1,1:3)/Lx*h + reclatvec(2,1:3)/Ly*l + reclatvec(3,1:3)/Lz*m
 			!ix = mod(h+10*Lx,2*Lx)
 			!iy = mod(l+10*Ly,2*Ly)
@@ -119,9 +139,9 @@ program structure_factor
 			c = CMPLX(0.d0,0.d0)
 			dq = reclatvec(1,1:3)/Lx*(h-ix) + reclatvec(2,1:3)/Ly*(l-iy) + reclatvec(3,1:3)/Lz*(m-iz)
 			do sb2=1,subl; do sb1=1,subl
-				!br = dot_product(dq,sublatvec(sb1,1:3)-sublatvec(sb2,1:3))
-				br = dot_product(q,sublatvec(sb1,1:3)-sublatvec(sb2,1:3))
-				c = c + SqSq(sb1,sb2,ix,iy,iz)*CMPLX(dcos(-br),dsin(-br))
+				!br = -dot_product(dq,sublatvec(sb1,1:3)-sublatvec(sb2,1:3))
+				br = -dot_product(q,sublatvec(sb1,1:3)-sublatvec(sb2,1:3))
+				c = c + SqSq(iGN,sb1,sb2,ix,iy,iz)*CMPLX(dcos(br),dsin(br))
 			end do; end do
 			!-------------------
 			if( fm==0 ) then
@@ -132,7 +152,7 @@ program structure_factor
 
 			c = CMPLX(0.d0,0.d0)
 			do sb1=1, subl
-				c = c + SqSq(sb1,sb1,ix,iy,iz)
+				c = c + SqSq(iGN,sb1,sb1,ix,iy,iz)
 			end do
 			c = c/subl
 			!-------------------
@@ -141,6 +161,24 @@ program structure_factor
 			end if
 			!-------------------
 			write(2,'(5ES12.4)') q(1),q(2),q(3),real(c),aimag(c)
+
+			ix=h; iy=l; iz=m
+			if(   GN>0  .and.             &
+				  ix>=0 .and. ix<Lx .and. &
+				  iy>=0 .and. iy<Ly .and. &
+				  iz>=0 .and. iz<Lz       &
+				 ) then
+				write(3,'(4I4)') ix+iy*Lx+iz*Lx*Ly, ix, iy, iz
+				do iGN=0, GN
+					c = CMPLX(0.d0,0.d0)
+					do sb2=1,subl; do sb1=1,subl
+						!br = -dot_product(dq,sublatvec(sb1,1:3)-sublatvec(sb2,1:3))
+						br = -dot_product(q,sublatvec(sb1,1:3)-sublatvec(sb2,1:3))
+						c = c + SqSq(iGN,sb1,sb2,ix,iy,iz)*CMPLX(dcos(br),dsin(br))
+					end do; end do
+					write(3,'(ES12.4)') real(c)
+				end do
+			end if
 		end do
 		write(1,'()')
 		write(2,'()')
@@ -148,4 +186,5 @@ program structure_factor
 	end do
 	close(1)
 	close(2)
+	if( GN>0 ) close(3)
 END program structure_factor
